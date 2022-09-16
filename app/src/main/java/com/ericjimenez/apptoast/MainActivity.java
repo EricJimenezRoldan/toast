dcafa7b2361475a020f03cd4f7370625c17eab19
EricJimenezRoldan <ejimenezroldan.cf@iesesteveterradas.cat>
2022-09-16 19:11:09 +0200

    toast Eric Jimenez

---
+++ app/src/main/java/com/ericjimenez/apptoast/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ericjimenez.apptoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+    }
+}
